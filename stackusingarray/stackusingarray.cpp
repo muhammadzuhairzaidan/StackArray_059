16a60a1b591a73cf7c54eba254b33395e6905c94
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-06-16 18:42:34 +0700

    prosedur push untuk menambahkan element

--- stackusingarray/stackusingarray.cpp
+++ stackusingarray/stackusingarray.cpp
@@ -13,3 +13,18 @@ public:
 	stackArray() {
 		top = -1;
 	}
+
+	void push() {
+		int element;
+		cout << "Enter element : ";
+		cin >> element;
+
+		if (top == 4) {
+			cout << "\nNumber of data exceed the limit" << endl; //step 1
+			return;
+		}
+		top++; //step 2
+		stack_array[top] = element; //step 3
+		cout << endl;
+		cout << element << " ditambahkan(pushed)" << endl;
+	}
